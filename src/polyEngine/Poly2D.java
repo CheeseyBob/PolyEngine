cd866cf675db766c8d5258738aedce8b4caa8c1f
CheeseyBob <cheeseybobdev@gmail.com>
2023-07-11 22:12:43 +0100

    Update formatting.

--- src/polyEngine/Poly2D.java
+++ src/polyEngine/Poly2D.java
@@ -6,10 +6,10 @@ import java.awt.geom.Point2D;
 import maths.M;
 
 public class Poly2D {
-	private Line2D.Double[] edgeList;
-	private Point2D.Double centre = new Point2D.Double(0, 0);
+	private final Line2D.Double[] edgeList;
+	private final Point2D.Double centre = new Point2D.Double(0, 0);
 	private double rMax;
-	
+
 	private static Line2D.Double[] toEdgeList(double[] xList, double[] yList){
 		Line2D.Double[] edgeList = new Line2D.Double[xList.length];
 		for(int i = 1; i < xList.length; i ++){
@@ -18,7 +18,7 @@ public class Poly2D {
 		edgeList[xList.length - 1] = new Line2D.Double(xList[xList.length - 1], yList[xList.length - 1], xList[0], yList[0]);
 		return edgeList;
 	}
-	
+
 	public Poly2D(Line2D.Double[] edgeList, double xCentre, double yCentre){
 		centre.x = xCentre;
 		centre.y = yCentre;
@@ -33,15 +33,15 @@ public class Poly2D {
 			rMax = Math.max(rMax, Math.max(Math.sqrt(dx1*dx1 + dy1*dy1), Math.sqrt(dx2*dx2 + dy2*dy2)));
 		}
 	}
-	
+
 	public Poly2D(double[] xList, double[] yList){
 		this(xList, yList, M.mean(xList), M.mean(yList));
 	}
-	
+
 	public Poly2D(double[] xList, double[] yList, double xCentre, double yCentre){
 		this(toEdgeList(xList, yList), xCentre, yCentre);
 	}
-	
+
 	public Poly2D clone(){
 		Line2D.Double[] edgeList = new Line2D.Double[this.edgeList.length];
 		for(int i = 0; i < edgeList.length; i ++){
@@ -49,25 +49,24 @@ public class Poly2D {
 		}
 		return new Poly2D(edgeList, centre.x, centre.y);
 	}
-	
+
 	public void draw(Graphics2D g){
-		for(int i = 0; i < edgeList.length; i ++){
-			g.drawLine((int)edgeList[i].x1, (int)edgeList[i].y1, (int)edgeList[i].x2, (int)edgeList[i].y2);
+		for (Line2D.Double edge : edgeList) {
+			g.drawLine((int) edge.x1, (int) edge.y1, (int) edge.x2, (int) edge.y2);
 		}
 	}
-	
+
 	public Point2D getCentre(){
 		return new Point2D.Double(centre.x, centre.y);
 	}
-	
+
 	/**
-	 * Returns the radius for a circle which bounds this polygon. 
-	 * @return
+	 * Returns the radius for a circle which bounds this polygon.
 	 */
 	public double getRadius(){
 		return rMax;
 	}
-	
+
 	public boolean isCollidingWith(Poly2D mesh){
 		if(centre.distanceSq(mesh.centre) < (rMax + mesh.rMax)*(rMax + mesh.rMax)){
 			for(Line2D edge: edgeList){
@@ -81,70 +80,70 @@ public class Poly2D {
 		}
 		return false;
 	}
-	
+
 	public void obscureView(double viewX, double viewY, double distance, Graphics2D g){
 		int[] xList = new int[4];
 		int[] yList = new int[4];
-		for(int i = 0; i < edgeList.length; i ++){
-			if(Math.min(edgeList[i].getP1().distanceSq(viewX, viewY), edgeList[i].getP2().distanceSq(viewX, viewY)) < distance*distance){
-				xList[0] = (int)edgeList[i].x1;
-				yList[0] = (int)edgeList[i].y1;
-				xList[1] = (int)edgeList[i].x2;
-				yList[1] = (int)edgeList[i].y2;
-				
-				double dx = edgeList[i].x2 - viewX;
-				double dy = edgeList[i].y2 - viewY;
-				double d = Math.sqrt(dx*dx + dy*dy);
-				dx = distance*dx/d;
-				dy = distance*dy/d;
-				xList[2] = (int)(edgeList[i].x2 + dx);
-				yList[2] = (int)(edgeList[i].y2 + dy);
-				
-				dx = edgeList[i].x1 - viewX;
-				dy = edgeList[i].y1 - viewY;
-				d = Math.sqrt(dx*dx + dy*dy);
-				dx = distance*dx/d;
-				dy = distance*dy/d;
-				xList[3] = (int)(edgeList[i].x1 + dx);
-				yList[3] = (int)(edgeList[i].y1 + dy);
-				
+		for (Line2D.Double edge : edgeList) {
+			if (Math.min(edge.getP1().distanceSq(viewX, viewY), edge.getP2().distanceSq(viewX, viewY)) < distance * distance) {
+				xList[0] = (int) edge.x1;
+				yList[0] = (int) edge.y1;
+				xList[1] = (int) edge.x2;
+				yList[1] = (int) edge.y2;
+
+				double dx = edge.x2 - viewX;
+				double dy = edge.y2 - viewY;
+				double d = Math.sqrt(dx * dx + dy * dy);
+				dx = distance * dx / d;
+				dy = distance * dy / d;
+				xList[2] = (int) (edge.x2 + dx);
+				yList[2] = (int) (edge.y2 + dy);
+
+				dx = edge.x1 - viewX;
+				dy = edge.y1 - viewY;
+				d = Math.sqrt(dx * dx + dy * dy);
+				dx = distance * dx / d;
+				dy = distance * dy / d;
+				xList[3] = (int) (edge.x1 + dx);
+				yList[3] = (int) (edge.y1 + dy);
+
 				g.fillPolygon(xList, yList, 4);
 			}
 		}
 	}
-	
+
 	public void translate(double dx, double dy){
-		for(int i = 0; i < edgeList.length; i ++){
-			edgeList[i].x1 += dx;
-			edgeList[i].y1 += dy;
-			edgeList[i].x2 += dx;
-			edgeList[i].y2 += dy;
+		for (Line2D.Double edge : edgeList) {
+			edge.x1 += dx;
+			edge.y1 += dy;
+			edge.x2 += dx;
+			edge.y2 += dy;
 		}
 		centre.x += dx;
 		centre.y += dy;
 	}
-	
+
 	public void rotate(double angle){
-		for(int i = 0; i < edgeList.length; i ++){
-			double x = edgeList[i].x1 - centre.x;
-			double y = edgeList[i].y1 - centre.y;
-			double r = Math.sqrt(x*x + y*y);
+		for (Line2D.Double edge : edgeList) {
+			double x = edge.x1 - centre.x;
+			double y = edge.y1 - centre.y;
+			double r = Math.sqrt(x * x + y * y);
 			double angle1 = Math.atan2(y, x);
 			angle1 += angle;
-			edgeList[i].x1 = (r*Math.cos(angle1)) + centre.x;
-			edgeList[i].y1 = (r*Math.sin(angle1)) + centre.y;
-			
-			x = edgeList[i].x2 - centre.x;
-			y = edgeList[i].y2 - centre.y;
-			
-			r = Math.sqrt(x*x + y*y);
+			edge.x1 = (r * Math.cos(angle1)) + centre.x;
+			edge.y1 = (r * Math.sin(angle1)) + centre.y;
+
+			x = edge.x2 - centre.x;
+			y = edge.y2 - centre.y;
+
+			r = Math.sqrt(x * x + y * y);
 			angle1 = Math.atan2(y, x);
 			angle1 += angle;
-			edgeList[i].x2 = (float)(r*Math.cos(angle1)) + centre.x;
-			edgeList[i].y2 = (float)(r*Math.sin(angle1)) + centre.y;
+			edge.x2 = (float) (r * Math.cos(angle1)) + centre.x;
+			edge.y2 = (float) (r * Math.sin(angle1)) + centre.y;
 		}
 	}
-	
+
 	public void setCentre(Point2D p){
 		centre.setLocation(p);
 	}
